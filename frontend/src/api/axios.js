450c9fc4b6e382f0463f62a66892e203426c22f3
Nikhil3405 <nikhiln432005@gmail.com>
2026-09-18 16:40:48 +0530

    Update API base URL to new endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://agrishare-52sg.onrender.com/api",
+  baseURL: "https://agri-share-h4c9.vercel.app/api",
 });
 
 // Attach access token
